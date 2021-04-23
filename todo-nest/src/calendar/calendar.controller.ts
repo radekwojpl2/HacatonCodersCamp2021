9263f3d8a4d8b5ae5368d10962ff9a807c75c95c
mateugazi <mateugazi182@outlook.com>
2021-04-23 22:56:15 +0200

    Added swagger config

--- todo-nest/src/calendar/calendar.controller.ts
+++ todo-nest/src/calendar/calendar.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 import CalendarService from './calendar.service';
 
 @Controller('calendar')
@@ -20,6 +21,29 @@ export default class CalendarController {
     } 
 
     @Post()
+    @ApiBody({
+        schema: {
+          type: 'object',
+          properties: {
+            startDate : {
+                type: 'string',
+                example: '2020-04-04'
+            },
+            endDate : {
+                type: 'string',
+                example: '2020-04-05'
+            },
+            title : {
+                type: 'string',
+                example: 'some title'
+            },
+            type : {
+                type: 'string',
+                example: 'normal'
+            }
+          },
+        },
+      })
     async addCalendarEvent(
         @Body('startDate') startDate: string,
         @Body('endDate') endDate: string,
